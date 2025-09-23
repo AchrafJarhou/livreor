69a996606f4b0faf1720969a6d55c5bd5739daf8
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-09-23 11:45:24 +0200

    initialisation projet livre d'or avec creation table users et commentaire

---
+++ livreor.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS `livreor` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `livreor`;
+
+
+-- Table utilisateurs
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`login` VARCHAR(255) NOT NULL UNIQUE,
+`password` VARCHAR(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- Table commentaires
+CREATE TABLE IF NOT EXISTS `commentaires` (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`commentaire` TEXT NOT NULL,
+`id_utilisateur` INT NOT NULL,
+`date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateurs`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
